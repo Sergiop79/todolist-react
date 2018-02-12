994764ab031edc337ca92badf9a96a93a6a00e7e
Sergio Peña <sergiop79@gmail.com>
2018-02-12 18:01:10 +0100

    Added item edition

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -7,11 +7,16 @@ class TodoItem extends Component {
     this.state = {
       id: this.props.id,
       name: this.props.name,
-      completed: this.props.completed
+      completed: this.props.completed,
+      editable: false
     }
 
     this.handleCheck = this.handleCheck.bind(this)
     this.deleteItem = this.deleteItem.bind(this)
+    this.handleLabelClick = this.handleLabelClick.bind(this)
+    this.handleEditChange = this.handleEditChange.bind(this)
+    this.handleEditEnter = this.handleEditEnter.bind(this)
+    this.handleBlur = this.handleBlur.bind(this)
   }
 
   handleCheck (e) {
@@ -26,12 +31,53 @@ class TodoItem extends Component {
     this.props.onDelete(id)
   }
 
+  handleLabelClick (e) {
+    e.preventDefault()
+    if(!this.state.completed) {
+      this.setState({ editable: !this.state.editable })
+    }
+  }
+
+  handleEditChange (e) {
+    this.setState({ name: e.target.value })
+  }
+
+  handleEditEnter (e) {
+    const ENTER_KEY = 13
+    if (e.keyCode === ENTER_KEY) {
+      this.setState({ editable: false })
+      this.props.onCompleted(this.state)
+    }
+  }
+
+  handleBlur (e) {
+    this.setState({ editable: false })
+    this.props.onCompleted(this.state)
+  }
+
+  _renderEditable () {
+    if (this.state.editable) {
+      return (
+        <input
+          autoFocus
+          onChange={this.handleEditChange}
+          onKeyUp={this.handleEditEnter}
+          onBlur={this.handleBlur}
+          type='text'
+          value={this.state.name}
+        />
+      )
+    } else {
+      return <label onClick={this.handleLabelClick}>{this.state.name}</label>
+    }
+  }
+
   render () {
-    const {name, id} = this.props
+    const {id} = this.props
     return (
       <li>
         <input id={id} checked={this.state.completed} type='checkbox' onChange={this.handleCheck} />
-        <label htmlFor={id}>{name}</label>
+        { this._renderEditable() }
         <button onClick={this.deleteItem}>X</button>
       </li>
     )
